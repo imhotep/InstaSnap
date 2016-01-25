fbc88eb4206a46e7a78eac58a86898e35ab93d36
Anis Kadri <anis@apache.org>
2016-01-25 15:36:47 -0800

    step 3 updating UI/storyboard

--- InstaSnap/ViewController.h
+++ InstaSnap/ViewController.h
@@ -7,9 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Cordova/CDVViewController.h>
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVViewController.h>
+#import <AVFoundation/AVCaptureSession.h>
+#import <AVFoundation/AVCaptureVideoPreviewLayer.h>
+#import <AVFoundation/AVMediaFormat.h>
+#import <AVFoundation/AVCaptureInput.h>
+#import <AVFoundation/AVCaptureOutput.h>
+#import <AVFoundation/AVVideoSettings.h>
+#import <ImageIO/ImageIO.h>
 
-@interface ViewController : UIViewController
 
-
+@interface ViewController : CDVViewController
+@property(nonatomic, weak) IBOutlet UIView *vImagePreview;
+@property (weak, nonatomic) IBOutlet UIButton *surpriseBtn;
+@property(nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
+- (void)captureNow:(void (^)(NSData*))sendImageToJS;
 @end
 
